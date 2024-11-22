038a9a2f24bf5afacd289da777ac6f461d943782
Susan Mwiberi <susan.mwiberi@gmail.com>
2024-11-22 08:55:57 +0300

    Added data tabulation for expenses

--- week1/expense-tracker/src/components/ExpenseList.js
+++ week1/expense-tracker/src/components/ExpenseList.js
@@ -1,52 +1,92 @@
 import React from "react";
 import PropType from "prop-types";
+import { PieChart } from "react-minimal-pie-chart";
 
 function ExpenseList(props) {
-  const { expenses, onClickDelete, onClickEdit, onClickCreate } = props;
+  const {
+    savings,
+    totalIncome,
+    totalExpenses,
+    expenses,
+    onClickDelete,
+    onClickEdit,
+    onClickCreate,
+  } = props;
+
   return (
     <React.Fragment>
-      <table>
-        <thead>
-          <tr>
-            <td>DATE</td>
-            <td>AMOUNT</td>
-            <td>DESCRIPTION</td>
-            <td>TYPE</td>
-            <td>ACTIONS</td>
-          </tr>
-        </thead>
-        <tbody>
-          {expenses.map((expense) => (
-            <tr key={expense.id}>
-              <td>{expense.expenseDate}</td>
-              <td>{expense.amount}</td>
-              <td>{expense.description}</td>
-              <td>{expense.expenseType}</td>
-              <td>
-                <button className="button" onClick={() => onClickEdit(expense)}>
-                  EDIT
-                </button>
-                <button
-                  className="button"
-                  style={{ backgroundColor: "hotpink" }}
-                  onClick={() => onClickDelete(expense.id)}
-                >
-                  DELETE
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div style={{ textAlign: "center" }}>
+        <div className="piechart">
+          <p
+            style={{
+              fontSize: "large",
+              fontWeight: "bolder",
+              color: savings < 0 ? "purple" : "blue",
+            }}
+          >
+            Total savings: {savings}
+          </p>
+          <PieChart
+            label={({ dataEntry }) =>
+              dataEntry.title + ":" + `${Math.round(dataEntry.percentage)} %`
+            }
+            labelStyle={{ fontSize: "30%" }}
+            labelPosition={50}
+            data={[
+              { title: "INCOME", value: totalIncome, color: "white" },
+              { title: "EXPENSES", value: totalExpenses, color: "purple" },
+            ]}
+          />
+        </div>
 
-      <button className="button" onClick={() => onClickCreate()}>
-        Add Expense
-      </button>
+        <table>
+          <thead>
+            <tr>
+              <td>DATE</td>
+              <td>AMOUNT</td>
+              <td>DESCRIPTION</td>
+              <td>TYPE</td>
+              <td>ACTIONS</td>
+            </tr>
+          </thead>
+          <tbody>
+            {expenses.map((expense) => (
+              <tr key={expense.id}>
+                <td>{expense.expenseDate}</td>
+                <td>{expense.amount}</td>
+                <td>{expense.description}</td>
+                <td>{expense.expenseType}</td>
+                <td>
+                  <button
+                    className="button"
+                    onClick={() => onClickEdit(expense)}
+                  >
+                    EDIT
+                  </button>
+                  <button
+                    className="button"
+                    style={{ backgroundColor: "hotpink" }}
+                    onClick={() => onClickDelete(expense.id)}
+                  >
+                    DELETE
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <button className="button" onClick={() => onClickCreate()}>
+          Add Expense
+        </button>
+      </div>
     </React.Fragment>
   );
 }
 
 ExpenseList.propTypes = {
+  savings: PropType.number,
+  totalIncome: PropType.number,
+  totalExpenses: PropType.number,
   expenses: PropType.array,
   onClickEdit: PropType.func,
   onClickDelete: PropType.func,
