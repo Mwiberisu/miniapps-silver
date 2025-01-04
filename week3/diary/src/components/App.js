9e0d7848c2186aa7244ac377233368c41a9d3836
Susan Mwiberi <smwiberi@safaricom.co.ke>
2025-01-04 08:21:08 +0300

    Base code for diary app

---
+++ week3/diary/src/components/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import DiaryEntry from "./DiaryEntry";
+function App() {
+  return (
+    <div className="App">
+      <DiaryEntry />
+    </div>
+  );
+}
+
+export default App;
---
+++ week3/diary/src/components/DiaryEntry.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function DiaryEntry() {
+  return (
+    <div className="diary-entry">
+      <h2>Title</h2>
+      <p>Content</p>
+    </div>
+  );
+}
+
+export default DiaryEntry;
