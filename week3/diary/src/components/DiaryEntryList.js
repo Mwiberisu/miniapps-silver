481a67b552afcce8c5dd94878e013b9e9c8ce84e
Susan Mwiberi <smwiberi@safaricom.co.ke>
2025-01-05 21:02:54 +0300

    Added filtering records per user

--- week3/diary/src/components/DiaryEntryList.js
+++ week3/diary/src/components/DiaryEntryList.js
@@ -1,27 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { firestore } from "./firebase";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { UserAuth } from "./UserAuth";
 
 function DiaryEntryList() {
   let navigate = useNavigate();
   const [diaryEntries, setDiaryEntries] = useState([]);
-  const { isSignedIn } = UserAuth();
+  const { user, isSignedIn } = UserAuth();
 
   useEffect(() => {
     fetchDiaryEntries();
-  }, []);
+  });
 
   const fetchDiaryEntries = async () => {
     try {
-      const diaryRef = await getDocs(collection(firestore, "diary_entries"));
+      const q = query(
+        collection(firestore, "diary_entries"),
+        where("userId", "==", user.uid)
+      );
+      const diaryRef = await getDocs(q);
 
       const fetchedDiaryEntries = diaryRef.docs.map((doc) => ({
         id: doc.id,
         ...doc.data(),
       }));
-      console.log(fetchedDiaryEntries);
       setDiaryEntries(fetchedDiaryEntries);
     } catch (error) {
       alert(error.message);
