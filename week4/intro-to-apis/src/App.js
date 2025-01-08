edb4195d2809913e7feb2a66a93b3e5f1a741884
Susan Mwiberi <smwiberi@safaricom.co.ke>
2025-01-08 10:49:34 +0300

    Added dark/light modes

--- week4/intro-to-apis/src/App.js
+++ week4/intro-to-apis/src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
 import FetchData from "./components/fetch_data";
+import { useState, createContext } from "react";
 
 function App() {
+  const ThemeContext = createContext();
+
+  const [theme, setTheme] = useState("light");
+
+  const className = theme === "light" ? "App" : "AppDark";
+
   document.title = "LePosts";
   return (
-    <div className="App">
+    <div className={className}>
+      <ThemeContext.Provider value={{ theme }}>
+        <div>
+          <button
+            onClick={() => {
+              setTheme(theme === "dark" ? "light" : "dark");
+            }}
+          >
+            {theme === "light" ? "USE DARK MODE" : "USE LIGHT MODE"}
+          </button>
+        </div>
+      </ThemeContext.Provider>
+
       <FetchData />
     </div>
   );
